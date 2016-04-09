c5855abc88606a0b45da000cb2e2801bea74e4a9
NeoScript <neoscripting@gmail.com>
2016-04-09 18:18:02 -0500

    updating read-prototype

--- Helpers/read-prototype.asm
+++ Helpers/read-prototype.asm
@@ -2,8 +2,9 @@
 
 .data
 file_loc: .asciiz "test.asm" #note: when launching from commandline, test.asm should be within the same folder as Mars.jar else it will default to base of the c:/ drive
-buffer: .space 1024
+buffer: .space 1
 new_line: .asciiz "\n"
+currInput: .space 20
 
 #error strings
 readErrorMsg: .asciiz "\nError in reading file\n"
@@ -12,6 +13,9 @@ openErrorMsg: .asciiz "\nError in opening file\n"
 .text
 main:
 	jal openFile
+	jal readTillSpace
+	jal printCurrInput
+	jal closeFile
 	j endProgram
 
 openFile:
@@ -23,19 +27,33 @@ openFile:
 	syscall
 	bltz $v0, openError		#if $v0 is less than 0, there is an error found
 	move $s0, $v0			#else save the file descriptor
-
-	#Read input from file
-	li $v0, 14			#syscall 14 - read filea
+	jr $ra
+	
+readTillSpace:
+	and $t1, $t1, $zero		#set $t1 to zero
+	beqz $t1, continueread		#if $t1 is zero continue to read
+	
+	continueread:
+	#Read input from file until space and store into currInput
+	li $v0, 14			#syscall 14 - read file
 	move $a0, $s0			#sets $a0 to file descriptor
 	la $a1, buffer			#stores read info into buffer
-	li $a2, 1024			#hardcoded size of buffer
+	li $a2, 1			#hardcoded size of buffer
 	syscall				
 	bltz $v0, readError		#if error it will go to read error
+					#else if no error continue working on method
+					
+					#if input is not a space, then add it to the currInput buffer
+					#else if input is a space, then disregard it and leave this method
+	jr $ra
 	
+printCurrInput:
 	li $v0, 4
 	la $a0, buffer
 	syscall
-
+	jr $ra
+	
+closeFile:
 	#Close the file 
 	li   $v0, 16       # system call for close file
 	move $a0, $s0      # file descriptor to close
